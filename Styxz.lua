60c8eea78790209aefbebb6411ea1e20c93b5b19
ToshyAPNS <kauenunes4549@gmail.com>
2024-12-29 23:28:59 -0300

    Update Styxz.lua

--- Styxz.lua
+++ Styxz.lua
@@ -16,7 +16,9 @@ local Scripts = {
     ["Blox Fruits"] = "https://raw.githubusercontent.com/ToshyWare/StyxzHub/refs/heads/main/BloxFruits.lua",
 }
 local IDs = {
-    [2753915549] or [4442272183] or [7449423635] = Scripts["Blox Fruits"],
+    [2753915549] = Scripts["Blox Fruits"],
+    [4442272183] = Scripts["Blox Fruits"],
+    [7449423635] = Scripts["Blox Fruits"],
 }
 
 if loadstring and IDs[ID] then
