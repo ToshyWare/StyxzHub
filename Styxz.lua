bdf8490901ce8bf2a6c372f1bd8f67ff175d7be1
ToshyAPNS <kauenunes4549@gmail.com>
2025-01-03 01:13:30 -0300

    Update Styxz.lua

--- Styxz.lua
+++ Styxz.lua
@@ -15,13 +15,16 @@ local ID = game.PlaceId
 local Scripts = {
     ["Blox Fruits"] = "https://raw.githubusercontent.com/ToshyWare/StyxzHub/refs/heads/main/BloxFruits.lua",
     ["Meme Sea"] = "https://raw.githubusercontent.com/ToshyWare/StyxzHub/refs/heads/main/MemeSea.lua",
+    ["Omini X"] = "https://raw.githubusercontent.com/ToshyWare/StyxzHub/refs/heads/main/OminiX.lua",
 }
 local IDs = {
     [2753915549] = Scripts["Blox Fruits"],
     [4442272183] = Scripts["Blox Fruits"],
     [7449423635] = Scripts["Blox Fruits"],
-  --<>--<>--<>--<>--<>--<>--<>--<>--
+  --<>--<>--<>--<>--<>--<>--<>--<>--<>--<>--
     [10260193230] = Scripts["Meme Sea"],
+  --<>--<>--<>--<>--<>--<>--<>--<>--<>--<>--
+    [5210095481] = Scripts["Omini X"],
 }
 
 if loadstring and IDs[ID] then
