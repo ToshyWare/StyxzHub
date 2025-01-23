377bbc335ea4a4f77c859dcb4e8cc06ca9a5f45a
ToshyAPNS <kauenunes4549@gmail.com>
2025-01-23 14:56:11 -0300

    Update Styxz.lua

--- Styxz.lua
+++ Styxz.lua
@@ -9,29 +9,4 @@
 :......::::::..::::::::..:::::..:::::..::........:::::..:::::..::
 ]]--
 
-local loadstring = loadstring or load
-local ID = game.PlaceId
-
-local Scripts = {
-    ["Blox Fruits"] = "https://raw.githubusercontent.com/ToshyWare/StyxzHub/refs/heads/main/BloxFruits.lua",
-    ["Meme Sea"] = "https://raw.githubusercontent.com/ToshyWare/StyxzHub/refs/heads/main/MemeSea.lua",
-    ["Omini X"] = "https://raw.githubusercontent.com/ToshyWare/StyxzHub/refs/heads/main/OminiX.lua",
-    ["Fisch"] = "https://raw.githubusercontent.com/ToshyWare/StyxzHub/refs/heads/main/Fisch.lua",
-}
-local IDs = {
-    [2753915549] = Scripts["Blox Fruits"],
-    [4442272183] = Scripts["Blox Fruits"],
-    [7449423635] = Scripts["Blox Fruits"],
-  --<>--<>--<>--<>--<>--<>--<>--<>--<>--<>--
-    [10260193230] = Scripts["Meme Sea"],
-  --<>--<>--<>--<>--<>--<>--<>--<>--<>--<>--
-    [5210095481] = Scripts["Omini X"],
-  --<>--<>--<>--<>--<>--<>--<>--<>--<>--<>--
-    [16732694052] = Scripts["Fisch"],
-}
-
-if loadstring and IDs[ID] then
-    loadstring(game:HttpGet(IDs[ID]))()
-else
-    warn("Game Not Found, No Script Was Executed")
-end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ToshyWare/StyxzHub/refs/heads/main/BloxFruits.lua"))()
